299920eeffae1e825c1f0e08ac6d1e43e09c0fa3
NirashaMadubhashini <nirashamadu0729@gmail.com>
2022-08-02 21:45:49 +0530

    update Product index.jsx

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,3 +1,14 @@
+import Grid from "@mui/material/Grid";
+import RubberBtn from "../../component/common/RubberBandBtn";
+import * as React from "react";
+
 export default function Product() {
+    return (
+        <div>
+            <Grid item lg={12} xs={12} sm={12} md={12} sx={{mt: 9, fontSize: 20}}>
+                <RubberBtn name="Product Manage"/>
+            </Grid>
+        </div>
+    )
 
 }
