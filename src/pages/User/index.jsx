df51fe5723ac66358dccc611363e81a25265bd13
NirashaMadubhashini <nirashamadu0729@gmail.com>
2022-08-02 21:47:41 +0530

    update User index.jsx

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,3 +1,13 @@
-export default function User() {
+import Grid from "@mui/material/Grid";
+import RubberBtn from "../../component/common/RubberBandBtn";
+import * as React from "react";
 
+export default function User() {
+    return (
+        <div>
+            <Grid item lg={12} xs={12} sm={12} md={12} sx={{mt: 9, fontSize: 20}}>
+                <RubberBtn name="User Registration"/>
+            </Grid>
+        </div>
+    )
 }
