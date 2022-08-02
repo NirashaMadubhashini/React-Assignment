273f73670825aadc5feeb3a7d610b21e5d28357f
NirashaMadubhashini <nirashamadu0729@gmail.com>
2022-08-02 18:01:47 +0530

    update App.js

--- src/App/App.js
+++ src/App/App.js
@@ -3,16 +3,23 @@ import {Route, Routes} from "react-router";
 import React from "react";
 import LogingSection from "../pages/Loging";
 import SignUp from "../pages/Loging/SignUp";
+import DashBoard from "../pages/DashBoard";
+import User from "../pages/User";
+import Product from "../pages/Product";
+import Cart from "../pages/cart";
+import MainDash from "../pages/Main/index";
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
           <Route exact path='/' element={<LogingSection/>}/>
-          {/*<Route exact path='admin' element={<AdminPanel/>}/>*/}
-          {/*<Route exact path='customer' element={<CustomerPanel/>}/>*/}
-          {/*<Route exact path='driver' element={<DriverPanel/>}/>*/}
           <Route exact path='signUp' element={<SignUp/>}/>
+          <Route exact path='main' element={<MainDash/>}/>
+          <Route exact path='dash' element={<DashBoard/>}/>
+          <Route exact path='user' element={<User/>}/>
+          <Route exact path='product' element={<Product/>}/>
+          <Route exact path='cart' element={<Cart/>}/>
         </Routes>
       </BrowserRouter>
 
