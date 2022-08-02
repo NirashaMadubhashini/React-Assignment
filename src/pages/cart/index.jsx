56396cccab6b9b51fcf371e8fe456dc234841f3e
NirashaMadubhashini <nirashamadu0729@gmail.com>
2022-08-02 21:44:58 +0530

    update Cart index.jsx

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -1,3 +1,13 @@
-export default function Cart() {
+import Grid from "@mui/material/Grid";
+import RubberBtn from "../../component/common/RubberBandBtn";
+import * as React from "react";
 
+export default function Cart() {
+    return (
+        <div>
+            <Grid item lg={12} xs={12} sm={12} md={12} sx={{mt: 9, fontSize: 20}}>
+                <RubberBtn name="Cart Manage"/>
+            </Grid>
+        </div>
+    )
 }
